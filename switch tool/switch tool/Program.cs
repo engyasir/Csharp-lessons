d6aae793be361b6d5894d159315755cca5711b8f
EngYasir <yasser7491@yahoo.com>
2016-10-05 22:48:55 +0300

    switch tool
    example

---
+++ switch tool/switch tool/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  engineer = "Yasser";
+
+            switch (engineer)
+            {
+                case "Yasser":
+                    Console.WriteLine("Engineer Yasser");
+                    break;
+                case "bye":
+                    Console.WriteLine("bye Yasser");
+                    break;
+                case "welcome":
+                    Console.WriteLine("welcom Yasser");
+                    break;
+                default:
+                    Console.WriteLine(" no one");
+                    break;
+            }
+            Console.ReadKey();
+
+            
+        }
+    }
+}
