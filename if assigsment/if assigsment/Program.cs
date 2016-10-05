9e783fd2c8ddf2084c42f7abc99bd13ab5cdd146
EngYasir <yasser7491@yahoo.com>
2016-10-05 18:17:58 +0300

    if assigment exmp
    example for if

---
+++ if assigsment/if assigsment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_assigsment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int my_x = 9 , my_y = 5;
+            if ((my_x <= 9) && (my_y == 5))
+            {
+                Console.WriteLine("the result is true ");
+
+            }
+            else
+            {
+                Console.WriteLine("the result is fulse");
+            }
+            Console.ReadKey();
+        }
+    }
+}
