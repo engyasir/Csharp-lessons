b081a3c8c479e308d9223a355b17cb0221bb0677
EngYasir <yasser7491@yahoo.com>
2016-10-05 23:22:41 +0300

    ternarry condition
    3 condition

---
+++ ternaray condition/ternaray condition/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ternaray_condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvar = 10;
+            string result = (myvar < 11) ? "less than 11" : "greater than 11";
+            Console.WriteLine(" the total is {0}", result);
+            Console.ReadKey();
+
+        }
+    }
+}
