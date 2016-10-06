bc400e031c0862159e2277957b7d66b2a0d75bd9
EngYasir <yasser7491@yahoo.com>
2016-10-06 18:23:42 +0300

    foor tool
    exampe multipline 5 rows and even and odd

---
+++ for tool/for tool/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   for (int i = 0; i <= 5; i++)
+               {
+                   for (int j = 0; j <= 9; j++)
+                   {
+                       Console.WriteLine(i + "*" + j + " = " + (i * j));
+
+                   }
+                   Console.WriteLine();
+               }
+                   Console.ReadKey();
+                   */
+            /*   for (int i = 0; i <= 5; i++)
+       {
+           for (int j = 0; j <= 9; j++)
+           {
+               Console.WriteLine(i + "*" + j + " = " + (i * j));
+
+           }
+           Console.WriteLine();
+       }
+           Console.ReadKey();
+           */
+
+          /*  for (int i = 1; i <= 6; i+=2)
+           {
+               for (int j = 0; j <= 9; j++)
+               {
+                   Console.WriteLine(i + "*" + j + " = " + (i * j));
+
+               }
+               Console.WriteLine();
+           }
+               Console.ReadKey();
+               */
+
+
+            for (int i = 0; i <= 6; i += 2)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine(i + "*" + j + " = " + (i * j));
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            // جدول ضرب الى حد الخمسةواعداد الفردية والزوجية مضروبة 
+
+        }
+    }
+}
