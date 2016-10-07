31fbe815f0924b640a6ab0d3b42269723f159c5f
EngYasir <yasser7491@yahoo.com>
2016-10-07 11:25:21 +0300

    convert
    example to convert

---
+++ convert/convert/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myfvar = 45.56f;
+            string mystringvar, mysubstring;
+            mystringvar = Convert.ToString(myfvar);
+            mysubstring = mystringvar.Substring(2,3);
+            Console.WriteLine($"the string value {mystringvar }");
+            Console.WriteLine($"the mysubstring value {mysubstring }");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
