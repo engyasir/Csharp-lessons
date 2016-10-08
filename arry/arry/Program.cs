d9d2c89d5061abe33cb5fe81b084cf9a1b123e38
EngYasir <yasser7491@yahoo.com>
2016-10-08 21:25:55 +0300

    arry tool
    example for arry

---
+++ arry/arry/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myArrysize = 6;
+            int[] mynumbers = new int[myArrysize] { 2, 5, 7, 8, 4 ,3 };
+
+            for (int i = 0; i < 6; i++)
+
+            Console.WriteLine("my number " + (i + 0) + " = " + $" {mynumbers[i]} " + " ");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
