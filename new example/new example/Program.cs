70d6d335b509d6a484de7e2bf8d28ce0a6a11105
EngYasir <yasser7491@yahoo.com>
2016-10-11 00:01:53 +0300

    new example
    test for A to Z

---
+++ new example/new example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int k = 0;
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                    Console.Write((j == (7 - k) || j == (7 + k)) ? (char)(65 + k) : ' ');
+
+                k += i < 7 ? 1 : -1;
+                Console.WriteLine();
+            }
+                    Console.ReadLine();
+        }
+
+            
+             
+     }
+    
+}
