bdb9059f8102718d891c1658f4da525e392ea71b
EngYasir <yasser7491@yahoo.com>
2016-10-11 23:53:47 +0300

    padleft and right
    example

---
+++ padright/padright/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace padright
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystr1, mystr2;
+
+            Console.WriteLine("enter the string :");
+            mystr1 = Console.ReadLine();
+
+            mystr2 = mystr1.PadRight(14,'e');
+            mystr2 = mystr2.PadRight(15,'s');
+            mystr2 = mystr2.PadRight(16,'t');
+            Console.WriteLine($"{mystr2}");
+            Console.ReadKey();
+        }
+    }
+}
