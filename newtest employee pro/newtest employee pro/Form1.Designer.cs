e431995028b0b3124b74de4e85cee957507c2cf7
EngYasir <yasser7491@yahoo.com>
2017-01-05 23:49:32 +0300

    برنامج قواعد بيانات موظفين
    عمليات البحث الاضافةوالحذف والتعديل والخزن

---
+++ newtest employee pro/newtest employee pro/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace newtest_employee_pro
+{
+    partial class frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmpID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmpSalary = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExpoData = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.databaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdAddress = new System.Windows.Forms.RadioButton();
+            this.rdName = new System.Windows.Forms.RadioButton();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EmployeeID";
+            // 
+            // txtEmpID
+            // 
+            this.txtEmpID.Location = new System.Drawing.Point(16, 51);
+            this.txtEmpID.Name = "txtEmpID";
+            this.txtEmpID.Size = new System.Drawing.Size(246, 26);
+            this.txtEmpID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "EmployeeName";
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(16, 102);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(246, 26);
+            this.txtEmpName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "EmployeeSalary";
+            // 
+            // txtEmpSalary
+            // 
+            this.txtEmpSalary.Location = new System.Drawing.Point(16, 153);
+            this.txtEmpSalary.Name = "txtEmpSalary";
+            this.txtEmpSalary.Size = new System.Drawing.Size(246, 26);
+            this.txtEmpSalary.TabIndex = 5;
+            this.txtEmpSalary.TextChanged += new System.EventHandler(this.txtEmpSalary_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(16, 210);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(246, 26);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.GridColor = System.Drawing.Color.LightGray;
+            this.dgv.Location = new System.Drawing.Point(12, 269);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.Size = new System.Drawing.Size(718, 188);
+            this.dgv.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(278, 75);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 28);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(278, 109);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 29);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(278, 144);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 27);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExpoData
+            // 
+            this.btnExpoData.Location = new System.Drawing.Point(278, 205);
+            this.btnExpoData.Name = "btnExpoData";
+            this.btnExpoData.Size = new System.Drawing.Size(198, 35);
+            this.btnExpoData.TabIndex = 12;
+            this.btnExpoData.Text = "Export Data To Xml";
+            this.btnExpoData.UseVisualStyleBackColor = true;
+            this.btnExpoData.Click += new System.EventHandler(this.btnExpoData_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.databaseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(742, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // databaseToolStripMenuItem
+            // 
+            this.databaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.databaseToolStripMenuItem.Name = "databaseToolStripMenuItem";
+            this.databaseToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.databaseToolStripMenuItem.Text = "Database";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.saveToolStripMenuItem.Text = "save into Database";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(278, 37);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(94, 32);
+            this.btnNew.TabIndex = 14;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdAddress);
+            this.groupBox1.Controls.Add(this.rdName);
+            this.groupBox1.Location = new System.Drawing.Point(442, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 62);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select field to Search";
+            // 
+            // rdAddress
+            // 
+            this.rdAddress.AutoSize = true;
+            this.rdAddress.Location = new System.Drawing.Point(119, 25);
+            this.rdAddress.Name = "rdAddress";
+            this.rdAddress.Size = new System.Drawing.Size(91, 23);
+            this.rdAddress.TabIndex = 1;
+            this.rdAddress.Text = "Address";
+            this.rdAddress.UseVisualStyleBackColor = true;
+            // 
+            // rdName
+            // 
+            this.rdName.AutoSize = true;
+            this.rdName.Checked = true;
+            this.rdName.Location = new System.Drawing.Point(6, 25);
+            this.rdName.Name = "rdName";
+            this.rdName.Size = new System.Drawing.Size(106, 23);
+            this.rdName.TabIndex = 0;
+            this.rdName.TabStop = true;
+            this.rdName.Text = "EmpName";
+            this.rdName.UseVisualStyleBackColor = true;
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(475, 41);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(198, 26);
+            this.txtsearch.TabIndex = 16;
+            this.txtsearch.TextChanged += new System.EventHandler(this.txtsearch_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(408, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 19);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Search";
+            // 
+            // sfd
+            // 
+            this.sfd.Filter = "xml file|*.xml";
+            // 
+            // frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(742, 469);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnExpoData);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmpSalary);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmpID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee";
+            this.Load += new System.EventHandler(this.frm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmpID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmpSalary;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExpoData;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem databaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdAddress;
+        private System.Windows.Forms.RadioButton rdName;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.SaveFileDialog sfd;
+    }
+}
+
---
+++ newtest employee pro/newtest employee pro/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+    
+namespace newtest_employee_pro
+{
+    public partial class frm : Form
+    {
+        // هنا جملة الاتصال والمتطلبات لكي نربط بين السيكوال سيرفر والسي شارب
+        // وايضا تعاريف مهمة من البيانات والجدول 
+        SqlConnection conn = new SqlConnection("Server=DESKTOP-U9D8BHS\\YASIR;Database=Emp;Integrated Security=True");
+        SqlDataAdapter adapter;
+        DataTable Dt = new DataTable("Employee");
+        DataSet ds = new DataSet();
+        SqlCommandBuilder cmdb;
+        CurrencyManager cm;
+        public frm()
+        {
+            // متطلبات الربط من جملة وجدول وملئ الداتا كرد فيو
+            InitializeComponent();
+            adapter = new SqlDataAdapter("select * from Employee", conn);
+            adapter.Fill(Dt);
+            dgv.DataSource = Dt;
+
+            // هنا كي نحمل المعلومات ونربطها بين التصميم التكستبوكس والجدول مع الداتاكردفيو
+            txtEmpID.DataBindings.Add("text", Dt, "EmployeeID");
+            txtEmpName.DataBindings.Add("text", Dt, "EmployeeName");
+            txtEmpSalary.DataBindings.Add("text", Dt, "EmployeeSalary");
+            txtAddress.DataBindings.Add("text", Dt, "Address");
+            // هذا الايعاز في الاسفل مهم يسهل علينا كتابة العمليات الاربعة من اضافة وحذف ... الخ
+            cm = (CurrencyManager)this.BindingContext[Dt];
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // زر الاضافة
+            cm.EndCurrentEdit();
+            cmdb = new SqlCommandBuilder(adapter);
+            adapter.Update(Dt);
+            MessageBox.Show("Add Successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            // زر تفريغ الخانات وعمل حقل جديد فارغ
+           cm.AddNew();
+           txtEmpID.Focus();
+        }
+
+        private void txtEmpSalary_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // زر التعديل 
+            cm.EndCurrentEdit();
+            cmdb = new SqlCommandBuilder(adapter);
+            adapter.Update(Dt);
+            cm.Refresh();
+            MessageBox.Show("Update Successfully!", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // زرالحذف 
+            // هنا رسالة قبل الحذف حيث يستفسر هل نريد فعلا الحذف او التراجع عن الحذف ؟
+            DialogResult del = MessageBox.Show("Are you sure to delete file", "worring", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (del == DialogResult.Yes)
+            {
+                cm.RemoveAt(cm.Position);
+                cm.EndCurrentEdit();
+                cmdb = new SqlCommandBuilder(adapter);
+                adapter.Update(Dt);
+                MessageBox.Show("Deleted Successfully!", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+            else
+            {
+                MessageBox.Show("Do Not Delete!", "Worring", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void txtsearch_TextChanged(object sender, EventArgs e)
+        {
+            // لغرض برمجة البحث عن طريق الاسم والعنوان 
+            DataView dv = new DataView(Dt);
+            string Expr= " like'%" + txtsearch.Text + "%'";
+
+                  
+                if (rdName.Checked) Expr = "EmployeeName" + Expr;
+
+                else if (rdAddress.Checked) Expr = "Address" + Expr;
+
+                dv.RowFilter = Expr;
+                dgv.DataSource = dv;
+            }
+           
+        
+
+        private void btnExpoData_Click(object sender, EventArgs e)
+        {
+            // في هذا الزر نقوم بحفظ البيانات على شكل فايل اكي ام ال حتى نستفاد منها عند حصول خطا او تلف قاعدة البيانات
+             if(sfd.ShowDialog()==DialogResult.OK)
+            {
+                Dt.WriteXml(sfd.FileName);
+                MessageBox.Show("File Saved");
+            }
+        }
+    }
+}
